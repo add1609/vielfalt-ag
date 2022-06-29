ad22f28bbf956b7bf59ef56bd20db486e69b5233
add003 <ahmedsami03@protonmail.com>
2022-06-29 02:15:50 +0200

    cleanup

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,14 +1,13 @@
 <script>
 	const imgList = [""];
 	const currentImg = imgList[0];
-
 </script>
 
 <div class="background-img hero" style:background-image={currentImg}>
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
         <div class="max-w-md">
-            <h1 class="mb-1 font-bold text-4xl md:mb-5 md:text-5xl">Lorem ipsum</h1>
+            <h1 class="font-bold text-4xl md:mb-5 md:text-5xl">Lorem ipsum</h1>
             <p class="text-lg md:text-lg">dolor sit amet, consectetur adipiscing elit.
                 Nullam id dolor eget nunc
                 ullamcorper blandit. </p>
